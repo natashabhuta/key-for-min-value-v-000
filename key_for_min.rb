b18f14da8b2df4166e2ae59c35a3d1ea681d1aee
natashabhuta <natasha.bhuta@gmail.com>
2018-08-10 14:32:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+name_hash = {:bitcoin => 7000, :ethereum => 400, :zcash => 390}
+if name_hash.length == 0
+    return nil
+  else 
 end
+end 
